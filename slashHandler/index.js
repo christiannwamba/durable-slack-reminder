988a5650e1fa645068323e251d2f610879175911
Christian Nwamba <nwambachristian@gmail.com>
2019-11-17 10:49:16 +0400

    Durable templates

---
+++ slashHandler/index.js
@@ -0,0 +1,34 @@
+﻿const df = require('durable-functions');
+const signature = require('../lib/verifySignature');
+
+module.exports = async function(context, req) {
+  context.log('headers', req.headers);
+  context.log('rawBody', req.rawBody);
+  context.log('body', req.body)
+
+//   if (!signature.isVerified(req)) {
+//     return handleUnVerifiedRequest(context);
+//   }
+
+  const client = df.getClient(context);
+  const instanceId = await client.startNew(
+    req.params.functionName,
+    undefined,
+    req.body
+  );
+  context.log(`Started orchestration with ID = '${instanceId}'.`);
+
+  return client.createCheckStatusResponse(context.bindingData.req, instanceId);
+};
+
+function handleUnVerifiedRequest(context) {
+  return (context.res = {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    status: 401,
+    body: {
+      message: 'getout'
+    }
+  });
+}
