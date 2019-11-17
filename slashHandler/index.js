8186aff3c4fd5dde6838c9b9b3d0dcbdf2e5dd00
Christian Nwamba <nwambachristian@gmail.com>
2019-11-17 15:42:01 +0400

    Parse url encoded body to json

--- slashHandler/index.js
+++ slashHandler/index.js
@@ -1,31 +1,30 @@
 ﻿const df = require('durable-functions');
+const axios = require('axios');
+var qs = require('querystring');
 const signature = require('../lib/verifySignature');
-const axios = require('axios')
 
 module.exports = async function(context, req) {
   context.log('headers', req.headers);
   context.log('rawBody', req.rawBody);
-  context.log('body', req.body)
-
-//   if (!signature.isVerified(req)) {
-//     return handleUnVerifiedRequest(context);
-//   }
-
-
+  context.log('body', req.body);
+  req.body = qs.parse(req.body);
+  //   if (!signature.isVerified(req)) {
+  //     return handleUnVerifiedRequest(context);
+  //   }
 
   const client = df.getClient(context);
-  const accessToken = process.env["SLACK_ACCESS_TOKEN"]
-  const userResponse = await axios.get(`https://slack.com/api/users.info?token=${accessToken}&user=${req.body.user_id}`)
-  const userTimeZone = userResponse.data.user.tz
-  
-  context.log('timezone>>>>>>>', userTimeZone)
+  const accessToken = process.env['SLACK_ACCESS_TOKEN'];
+  const userResponse = await axios.get(
+    `https://slack.com/api/users.info?token=${accessToken}&user=${req.body.user_id}`
+  );
+  const userTimeZone = userResponse.data.user.tz;
 
-
+  context.log('timezone>>>>>>>', userTimeZone);
 
   const instanceId = await client.startNew(
     req.params.functionName,
     undefined,
-    Object.assign(req.body, {timeZone: userTimeZone})
+    Object.assign(req.body, { timeZone: userTimeZone })
   );
   context.log(`Started orchestration with ID = '${instanceId}'.`);
 
